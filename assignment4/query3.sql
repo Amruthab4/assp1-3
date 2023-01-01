6ab9e6d27725896caea8cb9459fb1949d1d6b6e5
Amruthab4 <89182134+Amruthab4@users.noreply.github.com>
2023-01-01 22:56:04 +0530

    first commit

---
+++ assignment4/query3.sql
@@ -0,0 +1,9 @@
+INSERT INTO EmployeeDetails(Emp_id int,Emp_name varchar(100),Job_name varchar(200),Manager_id int,Hire_date int,
+Salary int,Dep_id int
+);
+
+VALUES (1001,'A','Manager',1,1-1-2023,60000,1001),(2001,'B','Assistant manager',2,2-1-2023,50000,1002),
+(3001,'C','Clerk',3,3-1-2023,40000,1003),(4001,'D','Assistant Clerk',4,4-1-2023,30000,1004),
+(5001,'E','Officer1',5,5-1-2023,5000,1005),(6001,'F','Officer2',6,6-1-2023,6000,1006),
+(7001,'G','Officer3',7,7-1-2023,7000,1007),(8001,'H','Officer4',8,8-1-2023,8000,1008),
+(9001,'I','Officer5',9,9-1-2023,9000,1009),(10010,'J','Officer6',10,10-1-2023,1000,1010);
